6440f02a380b23c3a7691fc9686ca8c9427543af
Anthony <anthonycolas.france@gmail.com>
2022-10-25 11:24:16 +0200

    Store: Fix

--- hr-net/src/app/store.js
+++ hr-net/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query/react';
-import { saveReducer } from '../features/saveEmployeeSlice';
+import saveReducer from '../features/saveEmployeeSlice';
 
 export const store = configureStore({
   reducer: {
