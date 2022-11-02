bb5e95b7a8ea88d5915e426f180f2769374c6e5c
Anthony <anthonycolas.france@gmail.com>
2022-11-02 15:39:12 +0100

    Feature: Redirect Home to Profile

--- hr-net/src/App.jsx
+++ hr-net/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Error from './pages/Error';
 import Profile from './pages/Profile';
 import EmployeeList from './pages/EmployeeList';
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Navigate replace to="/profile" />} />
         <Route path="/employee-list" element={<EmployeeList />} />
         <Route path="*" element={<Error />} />
       </Routes>
